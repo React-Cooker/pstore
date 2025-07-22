5e9eb43cb07093ace79aaa8b38c43ee5391e3b48
Ekrem <70305889+yakupozcan@users.noreply.github.com>
2025-07-22 15:04:48 +0300

    just clean project

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+  View,
+  Text,
+  SafeAreaView,
+  Platform,
+} from 'react-native';
+
+
+function App(): React.JSX.Element { const isDarkMode = useColorScheme() === 'dark'; // Sadece StatusBar barStyle için kullanılıyor
+return (
+    <SafeAreaView style={[styles.safeArea,{paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0, },]}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <View style={styles.contentContainer}>
+        <Text> just clean screen </Text>
+
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    paddingBottom: 0,
+    paddingHorizontal: 0,
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems:'center',
+  },
+});
+
+export default App;
